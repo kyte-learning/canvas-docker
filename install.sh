51956d59fa9ea1a31b8c55a940278daa838ec95d
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2017-12-20 16:16:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Set environment variables
 read -p "Set Account Name: "  ACCOUNT_NAME
 read -p "Set Admin Email: "  ADMIN_EMAIL
 read -s -p "Set Admin Password: "  ADMIN_PASSWORD
+
+# Create canvas-variables.env so we can pass them into docker-compose
+cat <<EOF > canvas-variables.env
+ACCOUNT_NAME=$ACCOUNT_NAME
+ADMIN_EMAIL=$ADMIN_EMAIL
+ADMIN_PASSWORD=$ADMIN_PASSWORD
+EOF
 
 # Get latest Docker Compose release
 latest=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep  '"name"' | head -n 1 | cut -d '"' -f 4)
@@ -11,4 +19,5 @@ latest=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | g
 curl -L --fail "https://github.com/docker/compose/releases/download/$latest/run.sh" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# Build, create, start, and attach to container
 docker-compose up -d
